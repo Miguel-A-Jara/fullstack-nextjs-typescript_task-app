27e5b6cd2502949b8cbe0f02521b5fd1f4801622
Miguel-A-Jara <0miguel.jara15@gmail.com>
2022-09-03 21:56:15 -0400

    Cambiando de 'todos' a 'tasks' en empty card

--- components/cards/EmptyCardList.tsx
+++ components/cards/EmptyCardList.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/Card/card.module.css';
 const EmptyCardList = () => {
   return (
     <div className={`${styles['card-empty']} app-shadow`}>
-      <h2 className='display-3 fw-bold'>Sorry, but no todos to show here!</h2>
+      <h2 className='display-3 fw-bold'>Sorry, but no tasks to show here!</h2>
     </div>
   )
 }
